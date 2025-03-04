7f405e7f090fc900a96e191bb6ea4ebf8d48abf1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-03-04 01:01:09 +0000

    Modify code to demo promises

--- 5. Promises/ejemplo_promesa.mjs
+++ 5. Promises/ejemplo_promesa.mjs
@@ -14,14 +14,18 @@ let miPromesita = new Promise((resolve, reject) => {
 
 function main() {
   console.log('Inicio de main');
+  setTimeout(() => { console.log('Primer timer.'); }, 200);
+  setTimeout(() => { console.log('Segundo timer.'); }, 500);
+  setTimeout(() => { console.log('Tercer timer.'); }, 100);
   miPromesita
-  .then((valorResuelto) => {
-    console.log('Cumplida');
-    console.log(valorResuelto);
-  }).catch((valorRechazado) => {
-    console.error('No cumplida');
-    console.log(valorRechazado);
-  });
+    .then((valorResuelto) => {
+      console.log('Cumplida');
+      console.log(valorResuelto);
+    })
+    .catch((valorRechazado) => {
+      console.error('No cumplida');
+      console.log(valorRechazado);
+    });
   console.log('fin de main');
 }
 
